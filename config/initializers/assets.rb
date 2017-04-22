12171ac0528be88f2ba139bce9b69e9a8ee370ba
Nguyen Kha <nlhkha@gmail.com>
2017-04-22 13:59:06 +0300

    add additional assets to initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( views/frontpage/show.js frontpage.css application-new.css )
+Rails.application.config.assets.precompile += %w( views/frontpage/show.js submissions.js bootstrap.js frontpage.css application-new.css )
