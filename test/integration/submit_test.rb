bfda292fad81d930cbfaa4720504c69c38865043
Neea Tuomainen <neea.tuomainen@aalto.fi>
2018-07-27 09:53:07 +0300

    add test for a+ submission

--- test/integration/submit_test.rb
+++ test/integration/submit_test.rb
@@ -42,8 +42,41 @@ class SubmitTest < ActionDispatch::IntegrationTest
 #     
 #     
 #   end
-  
-  
-  
-  
+
+  def setup
+    @instance = course_instances(:lti_instance)
+    @exercise = exercises(:lti_exercise)
+    @student1 = users(:lti_student1)
+    @student2 = users(:lti_student2)
+    @student3 = users(:lti_student3)
+  end
+
+  test "should be able to submit through A+" do
+    group1 = [{'user'=> @student1.lti_user_id, 'email'=> @student1.email, 'name'=> 'Student 1'}, 
+              {'user'=> @student3.lti_user_id, 'email'=> @student3.email, 'name'=> 'Student 3'}].to_json
+    group2 = [{'user'=> @student1.lti_user_id, 'email'=> @student1.email, 'name'=> 'Student 1'}, 
+              {'user'=> @student2.lti_user_id, 'email'=> @student2.email, 'name'=> 'Student 2'}].to_json
+
+    assert_no_difference 'Group.count',          "If group already exist, should not create a new group" do
+      assert_difference 'Submission.count', 1,   "Should be able to submit" do
+        post "/aplus/#{@exercise.id}", params: { exercise:             @exercise,              resource_link_id: @exercise.lti_resource_link_id, 
+                                                 oauth_consumer_key:   @instance.lti_consumer, context_id:       @instance.lti_context_id, 
+                                                 custom_group_members: group1,                 user_id:          @student1.lti_user_id }
+      end
+    end
+    assert_equal [@student1.id, @student3.id], Submission.last.group.group_members.map{|m| m.user_id},  "Group should have right members"
+    assert_equal @exercise, Submission.last.exercise,                                                   "Submission should be in right exercise"
+
+    assert_difference 'Group.count', 1,          "If group does not exist should create a new one" do
+      assert_difference 'Submission.count', 1,   "Should be able to submit" do
+        post "/aplus/#{@exercise.id}", params: { exercise:             @exercise,              resource_link_id: @exercise.lti_resource_link_id, 
+                                                 oauth_consumer_key:   @instance.lti_consumer, context_id:       @instance.lti_context_id, 
+                                                 custom_group_members: group2,                 user_id:          @student1.lti_user_id }
+      end
+    end
+    assert_equal [@student1.id, @student2.id], Submission.last.group.group_members.map{|m| m.user_id},  "Group should have right members"
+    assert_equal @exercise, Submission.last.exercise,                                                   "Submission should be in right exercise"
+    assert_equal [@student1.id, @student2.id], Group.last.group_members.map{|m| m.user_id},             "Group should have right members"
+  end
+    
 end
